541b3d077854ac6d083671109fa8d6b7a420fd5f
Lucas <lucasmartiniscool@gmail.com>
2023-11-22 19:27:48 -0800

    AHHHHHH

--- client.c
+++ client.c
@@ -10,63 +10,105 @@
 #define PORT 54321
 #define MAX_BURGERS 10
 
-void error(const char *msg) {
+void error(const char *msg)
+{
     perror(msg);
     exit(0);
 }
 
-int main(int argc, char *argv[]) {
+void validate_args(int argc, char *argv[]) {
+    char* default_hostname = "127.0.0.1";
+    int default_portno = 54321;
+    int default_burgers = 10;
+
+    if (argc < 4) {
+        fprintf(stderr, "Insufficient arguments provided. Using default values.\n");
+        argv[1] = default_hostname;
+        argv[2] = (char*) malloc(6);
+        sprintf(argv[2], "%d", default_portno);
+        argv[3] = (char*) malloc(4);
+        sprintf(argv[3], "%d", default_burgers);
+    }
+
+    int portno = atoi(argv[2]);
+    if (portno <= 0 || portno > 65535) {
+        fprintf(stderr, "Invalid port number. Using default value %d.\n", default_portno);
+        sprintf(argv[2], "%d", default_portno);
+    }
+
+    int burgers_wanted = atoi(argv[3]);
+    if (burgers_wanted <= 0 || burgers_wanted > 200) {
+        fprintf(stderr, "Invalid number of burgers. Using default value %d.\n", default_burgers);
+        sprintf(argv[3], "%d", default_burgers);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
+    validate_args(argc, argv);
+
+    char* hostname = argv[1];
     portno = atoi(argv[2]);
+    int burgers_wanted = atoi(argv[3]);
+    int burgers_eaten = 0;
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+
+    server = gethostbyname(hostname);
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr_list[0], 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr_list[0],
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
 
-    int burgers_eaten = 0;
-    int burgers_wanted = (argc > 3) ? atoi(argv[3]) : 10; // Use the command line argument if provided, otherwise default to 10
     // Send the number of burgers wanted to the server
     char buffer[256];
     sprintf(buffer, "%d", burgers_wanted);
     write(sockfd, buffer, strlen(buffer));
-    
-    while (burgers_eaten < burgers_wanted) {
-        bzero(buffer,256);
-        n = read(sockfd,buffer,255);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("%s\n",buffer);
-        if(strstr(buffer, "You got a burger!") != NULL) {
+
+    while (burgers_eaten < burgers_wanted)
+    {
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n <= 0)
+        {
+            if (n == 0)
+            {
+                printf("The restaurant is closing. Exiting...\n");
+            }
+            else
+            {
+                perror("ERROR reading from socket");
+            }
+            close(sockfd);
+            exit(0);
+        }
+
+        printf("%s\n", buffer);
+        if (strstr(buffer, "You got a burger!") != NULL)
+        {
             burgers_eaten++;
             printf("I have eaten %d burgers. I still want %d burgers.\n", burgers_eaten, burgers_wanted - burgers_eaten);
-        } else if (strstr(buffer, "The restaurant is closing.") != NULL) {
-            printf("The restaurant is closing. Exiting...\n");
-            break;
         }
     }
-
     printf("I no longer want burgers.\n");
-
     close(sockfd);
     return 0;
 }
-
--- server.c
+++ server.c
@@ -9,11 +9,17 @@
 #include <time.h>
 
 #define PORT 54321
-#define MAX_BURGERS 25
-#define MAX_CHEFS 2
+#define MAX_CLIENTS 200 // Maximum number of clients
+#define BUFFER_SIZE 256
+#define LISTEN_BACKLOG 5
+#define SLEEP_DURATION 4
 
+int client_sockets[MAX_CLIENTS]; // Array to store the client sockets
+int num_clients = 0;             // Counter for the number of clients
 int burgers = 0;
 pthread_mutex_t lock;
+int total_burgers_made = 0;
+int max_burgers = 0;
 
 void error(const char *msg)
 {
@@ -21,117 +27,200 @@ void error(const char *msg)
     exit(1);
 }
 
-int total_burgers_made = 0;
-
 void *chef(void *arg)
 {
+    int id = *((int *)arg);
     int burgers_made = 0;
+
     while (1)
     {
+        int time = rand() % 3 + 2;
         sleep(rand() % 3 + 2);
         pthread_mutex_lock(&lock);
-        if (total_burgers_made < MAX_BURGERS)
+        if (total_burgers_made < max_burgers)
         {
             burgers++;
             burgers_made++;
             total_burgers_made++;
-            printf("Chef %ld made a burger. Total burgers in stock: %d, Total burgers made: %d\n", (long)arg, burgers, total_burgers_made);
+            printf("\nChef %d made a burger in %d seconds. Burgers Available: %d, Burgers made: %d\n", id, time, burgers, total_burgers_made);
         }
         pthread_mutex_unlock(&lock);
-        if (total_burgers_made >= MAX_BURGERS)
+        if (total_burgers_made >= max_burgers)
         {
-            printf("All burgers are produced. The restaurant will remain open until all burgers are served.\n");
             break;
         }
     }
+
+    free(arg);
     return NULL;
 }
 
-typedef struct {
-    int sock;
-    int burgers_wanted;
-} client_info;
+void *client_handler(void *arg)
+{
+    int sock = *((int *)arg);
 
-void *client_handler(void *arg) {
-    client_info *info = (client_info *)arg;
-    int sock = info->sock;
     // Read the number of burgers wanted from the client
-    char buffer[256];
+    char buffer[BUFFER_SIZE];
     read(sock, buffer, 255);
     int burgers_wanted = atoi(buffer);
-    char message[256];
+
+    char message[BUFFER_SIZE];
     int burgers_served = 0;
-    while (1) {
+    while (1)
+    {
         sleep(rand() % 5 + 1);
         pthread_mutex_lock(&lock);
-        if (burgers > 0 && burgers_served < burgers_wanted) {
+        if (burgers > 0 && burgers_served < burgers_wanted)
+        {
             burgers--;
             burgers_served++;
             sprintf(message, "You got a burger! Burgers left: %d\n", burgers);
             printf("Serving a burger to client %d. Burgers left: %d. Client %d still wants %d burgers.\n", sock, burgers, sock, burgers_wanted - burgers_served);
             write(sock, message, strlen(message));
         }
+        else if (burgers == 0 && burgers_served == max_burgers)
+        {
+            // Break the loop if the restaurant has served all the burgers
+            break;
+        }
         pthread_mutex_unlock(&lock);
-        if (burgers_served >= burgers_wanted) {
+        if (burgers_served >= burgers_wanted)
+        {
             printf("Client %d no longer wants burgers.\n", sock);
             break;
         }
     }
+
     close(sock);
-    free(arg);
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+void *accept_clients(void *arg)
+{
+    int sockfd = *((int *)arg);
+    struct sockaddr_in cli_addr;
+    socklen_t clilen = sizeof(cli_addr);
+    while (1)
+    {
+        int *newsockfd = malloc(sizeof(int));
+        *newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (*newsockfd < 0)
+            error("ERROR on accept");
+        pthread_t client_thread;
+        pthread_create(&client_thread, NULL, client_handler, (void *)newsockfd);
+    }
+}
+
+void validate_args(int argc, char *argv[])
+{
+    int default_max_burgers = 25;
+    int default_num_chefs = 2;
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "Insufficient arguments provided. Using default values.\n");
+        argv[1] = (char *)malloc(4);
+        sprintf(argv[1], "%d", default_max_burgers);
+        argv[2] = (char *)malloc(3);
+        sprintf(argv[2], "%d", default_num_chefs);
+    }
+
+    int max_burgers = atoi(argv[1]);
+    if (max_burgers <= 0 || max_burgers > 1000)
+    {
+        fprintf(stderr, "Invalid number of burgers. Using default value %d.\n", default_max_burgers);
+        sprintf(argv[1], "%d", default_max_burgers);
+    }
+
+    int num_chefs = atoi(argv[2]);
+    if (num_chefs <= 0 || num_chefs > 50)
+    {
+        fprintf(stderr, "Invalid number of chefs. Using default value %d.\n", default_num_chefs);
+        sprintf(argv[2], "%d", default_num_chefs);
+    }
+}
+
+void handle_error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int create_socket()
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        handle_error("ERROR opening socket");
+
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+    {
+        handle_error("setsockopt");
+    }
+
+    return sockfd;
+}
+
+int main(int argc, char *argv[])
+{
+    validate_args(argc, argv);
+
+    max_burgers = atoi(argv[1]);
+    int num_chefs = atoi(argv[2]);
+
     srand(time(NULL));
     pthread_mutex_init(&lock, NULL);
 
-    pthread_t chef_thread[MAX_CHEFS];
-    for (long i = 0; i < MAX_CHEFS; i++) {
-        pthread_create(&chef_thread[i], NULL, chef, (void *)i);
+    pthread_t chef_thread[num_chefs];
+    for (long i = 0; i < num_chefs; i++)
+    {
+        int *id = malloc(sizeof(int));
+        *id = i;
+        pthread_create(&chef_thread[i], NULL, chef, (void *)id);
     }
 
-    int sockfd, newsockfd, portno;
-    socklen_t clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
+    int sockfd = create_socket();
+    int portno = PORT;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-
-    int opt = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = PORT;
-
+    struct sockaddr_in serv_addr;
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR on binding");
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        handle_error("ERROR on binding");
 
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
+    listen(sockfd, LISTEN_BACKLOG);
 
-    while (1) {
-        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0) 
-            error("ERROR on accept");
+    struct sockaddr_in cli_addr;
+    socklen_t clilen = sizeof(cli_addr);
 
-        pthread_t client_thread;
-        client_info *info = malloc(sizeof(client_info));
-        info->sock = newsockfd;
-        info->burgers_wanted = /* number of burgers this client wants */
-        pthread_create(&client_thread, NULL, client_handler, (void*) info);
+    pthread_t accept_thread;
+    pthread_create(&accept_thread, NULL, accept_clients, (void *)&sockfd);
+
+    while (1)
+    {
+        // Check if all burgers have been produced and served
+        if (total_burgers_made >= max_burgers && burgers == 0)
+        {
+            break;
+        }
     }
 
+    // Wait for all chef threads to finish
+    for (long i = 0; i < num_chefs; i++)
+    {
+        pthread_join(chef_thread[i], NULL);
+    }
+
+    // Wait for the accept thread to finish
+    pthread_join(accept_thread, NULL);
+    printf("The restaurant is closing.\n");
+
+    // Add a delay before closing the socket
+    sleep(SLEEP_DURATION);
+
     close(sockfd);
-    return 0; 
+    return 0;
 }
